309ed09fc1417ab3a2c7de3d28626a7a8697a34b
marcin <pieczka.marcin@gmail.com>
2017-06-11 12:53:38 +0200

    works slooow

--- test.py
+++ test.py
@@ -4,45 +4,56 @@ from time import sleep
 from tkinter import Tk, Label, Button
 import threading
 
-patterns = [0 for _ in range(8)]
+patterns = [40 for _ in range(8)]
 
 class MyFirstGUI:
-    def __init__(self, master):
+    def __init__(self, master, seq):
         self.master = master
+        master.minsize(width=600, height=400)
         master.title("A simple GUI")
+        master.bind('<Key>', self.key_press)
 
-        self.label = Label(master, text="This is our first GUI!")
+        self.label = Label(master, text="SickSeq")
         self.label.pack()
 
-        self.greet_button = Button(master, text="Greet", command=self.greet)
-        self.greet_button.pack()
-
-        self.close_button = Button(master, text="Close", command=master.quit)
-        self.close_button.pack()
-
-    def greet(self):
-        print("Greetings!")
+    def key_press(self, event):
+        global patterns
+        patterns = [x - 1 for x in patterns]
+        print(event)
 
 class Sequencer(threading.Thread):
-    def __init__(self, name):
+    def __init__(self):
         threading.Thread.__init__(self)
-        self.name = name
+        self.stop = False
 
     def run(self):
         alsaseq.client( 'Simple', 1, 1, False )
         alsaseq.start()
+        global patterns
 
         while True:
+            print('jestem')
+            if self.stop:
+                alsaseq.stop()
+                break
             for note in patterns:
-                note_tuple = (1, note, 100)
+                note_tuple = (0, note, 100)
+                print(noteonevent(*note_tuple))
+                #alsaseq.output((6, 1, 0, 1, (5, 0), (0, 0), (0, 0), (0, 60, 127, 0, 100)))
                 alsaseq.output(noteonevent(*note_tuple))
                 sleep(0.2)
                 alsaseq.output(noteoffevent(*note_tuple))
 
 
+
+def closing():
+    seq.stop = True
+    root.destroy()
+
 root = Tk()
-my_gui = MyFirstGUI(root)
-seq = Sequencer('seq')
+root.protocol('WM_DELETE_WINDOW', closing)
+seq = Sequencer()
+my_gui = MyFirstGUI(root, seq)
 seq.start()
 root.mainloop()
 seq.join()
